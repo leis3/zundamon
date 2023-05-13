77822defd238a2860c4ef1a3a091ab8feaf3232f
leis <uthmar33@gmail.com>
2023-05-13 21:21:25 +0900

    [feat] Implemented `/dictionary update` command

--- src/commands/dictionary/update.rs
+++ src/commands/dictionary/update.rs
@@ -1,8 +1,26 @@
 use std::sync::Arc;
+use std::collections::HashMap;
 use tokio::sync::RwLock;
-use crate::dictionary::Dictionary;
+use crate::dictionary::{Dictionary, DictionaryItem};
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::model::application::interaction::application_command::CommandDataOptionValue;
 
 pub async fn run(options: &[CommandDataOption], dict: Arc<RwLock<Dictionary>>) -> String {
-    unimplemented!()
+    let map = options.iter().map(|option| {
+        (option.name.as_str(), option.resolved.as_ref().unwrap())
+    }).collect::<HashMap<_, _>>();
+
+    let CommandDataOptionValue::String(key) = map["単語"].clone() else { panic!() };
+    let CommandDataOptionValue::String(value) = map["読み"].clone() else { panic!() };
+    let CommandDataOptionValue::Boolean(is_regex) = *map["正規表現"] else { panic!() };
+    let CommandDataOptionValue::Integer(priority) = *map["優先度"] else { panic!() };
+    let item = DictionaryItem { key, value, is_regex, priority };
+
+    let mut dict = dict.write().await;
+    if dict.update(item) {
+        "辞書を上書きしました。".into()
+    } else {
+        "指定した単語は辞書に登録されていません。".into()
+    }
+
 }
