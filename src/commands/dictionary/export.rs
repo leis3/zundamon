5c0554668d68d0c88f36ea000657fb4123fc6888
leis <uthmar33@gmail.com>
2023-05-15 12:34:33 +0900

    [feat] Implemented `/dictionary export` command

--- src/commands/dictionary/export.rs
+++ src/commands/dictionary/export.rs
@@ -1,8 +1,38 @@
+use crate::DictData;
 use serenity::prelude::*;
 use serenity::Result;
-use serenity::model::prelude::interaction::application_command::CommandDataOption;
-use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
+use serenity::model::channel::AttachmentType;
+use serenity::model::application::interaction::{
+    InteractionResponseType,
+    application_command::{
+        CommandDataOption,
+        CommandDataOptionValue,
+        ApplicationCommandInteraction
+    }
+};
 
 pub async fn run(options: &[CommandDataOption], ctx: &Context, interaction: &ApplicationCommandInteraction) -> Result<()> {
-    unimplemented!()
+    let CommandDataOptionValue::String(format) = options[0].resolved.as_ref().unwrap() else {
+        panic!()
+    };
+
+    match format.as_str() {
+        "JSON" => {
+            let dict = {
+                let data_read = ctx.data.read().await;
+                data_read.get::<DictData>().expect("Expected DictData in TypeMap.").clone()
+            };
+            let data = serde_json::to_string_pretty(&*dict.read().await.items).unwrap();
+
+            interaction.create_interaction_response(&ctx.http, |response| {
+                response.kind(InteractionResponseType::ChannelMessageWithSource)
+                    .interaction_response_data(|message| {
+                        message
+                            .add_file(AttachmentType::from((data.as_bytes(), "dictionary.json")))
+                            .ephemeral(true)
+                    })
+            }).await
+        },
+        _ => unreachable!("unsupported file format" )
+    }
 }
