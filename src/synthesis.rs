113b14b71f69a42b72b0dec11a2072fe1cac69bb
leis <uthmar33@gmail.com>
2023-05-23 15:48:39 +0900

    [clean] Changed `VoicevoxCore` object to static

--- src/synthesis.rs
+++ src/synthesis.rs
@@ -1,14 +1,19 @@
 use std::io::Write;
 use vvcore::*;
+use once_cell::sync::Lazy;
+
+static VOICEVOX_CORE: Lazy<VoicevoxCore> = Lazy::new(|| {
+    let dir = std::ffi::CString::new("voicevox_core/open_jtalk_dic_utf_8-1.11").unwrap();
+
+    let vvc = VoicevoxCore::new_from_options(AccelerationMode::Auto, 0, false, dir.as_c_str()).unwrap();
+    vvc.load_model(1).unwrap();
+
+    vvc
+});
 
 /// VOICEVOX COREで音声を合成する。
 pub fn synthesis(text: &str) -> Result<Vec<u8>, ResultCode> {
-    let dir = std::ffi::CString::new("voicevox_core/open_jtalk_dic_utf_8-1.11").unwrap();
-
-    let vvc = VoicevoxCore::new_from_options(AccelerationMode::Auto, 0, false, dir.as_c_str())?;
-    vvc.load_model(1).unwrap();
-
-    let wav = vvc.tts_simple(text, 1)?;
+    let wav = VOICEVOX_CORE.tts_simple(text, 1)?;
 
     let mut file = std::fs::File::create("temp.wav").unwrap();
     file.write_all(&mut wav.as_slice()).unwrap();
