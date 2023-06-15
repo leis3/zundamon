53084f3c6245ff63a0da7dcada14695474d4d363
leis <uthmar33@gmail.com>
2023-06-15 21:08:03 +0900

    [change] Changed speaker to normal zundamon

--- src/synthesis.rs
+++ src/synthesis.rs
@@ -14,14 +14,14 @@ static VOICEVOX_CORE: Lazy<VoicevoxCore> = Lazy::new(|| {
     let dir = std::ffi::CString::new("voicevox_core/open_jtalk_dic_utf_8-1.11").unwrap();
 
     let vvc = VoicevoxCore::new_from_options(AccelerationMode::Auto, 0, false, dir.as_c_str()).unwrap();
-    vvc.load_model(1).unwrap();
+    vvc.load_model(3).unwrap();
 
     vvc
 });
 
 /// VOICEVOX COREで音声を合成する。
 pub fn synthesis(text: &str) -> Result<Vec<u8>, ResultCode> {
-    let wav = VOICEVOX_CORE.tts_simple(text, 1)?;
+    let wav = VOICEVOX_CORE.tts_simple(text, 3)?;
 
     Ok(wav.as_slice().to_vec())
 }
