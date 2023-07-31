2245bf430ab5c18e9f0b673f4b6b990391c8c3fc
leis <uthmar33@gmail.com>
2023-08-01 06:59:02 +0900

    [fix] Changed to log to a new file when the date changes

--- core/src/log.rs
+++ core/src/log.rs
@@ -1,31 +1,27 @@
 use std::io::Write;
 use std::fs::File;
 use std::path::Path;
-use std::sync::Mutex;
 use chrono::Datelike;
-use once_cell::sync::Lazy;
 
-static LOG_FILE: Lazy<Mutex<File>> = Lazy::new(|| {
+fn open_log_file() -> std::io::Result<File> {
     let log_dir = Path::new("logs");
     if !log_dir.exists() {
         std::fs::create_dir(log_dir).unwrap();
     }
     let now = chrono::Utc::now().with_timezone(&chrono_tz::Japan);
     let filename = format!("{}-{:02}-{:02}.log", now.year(), now.month(), now.day());
-    let file = std::fs::OpenOptions::new()
+    std::fs::OpenOptions::new()
         .write(true)
         .create(true)
         .append(true)
         .open(log_dir.join(filename))
-        .unwrap();
-    Mutex::new(file)
-});
+}
 
 pub struct LogWriter;
 
 impl Write for LogWriter {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let mut file = LOG_FILE.lock().unwrap();
+        let mut file = open_log_file()?;
         writeln!(file, "{}", String::from_utf8(buf.to_vec()).unwrap())?;
         Ok(buf.len())
     }
