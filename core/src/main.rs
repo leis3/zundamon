eae6cfc3e56ff49a405967d0e67553e9c510f02f
leis <uthmar33@gmail.com>
2023-09-01 18:16:47 +0900

    [feat] Added ability to shutdown bot from console

--- core/src/main.rs
+++ core/src/main.rs
@@ -11,14 +11,15 @@ use event_handler::Handler;
 use type_map::{TextChannelId, ConfigData, ConnectedChannel};
 use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
+use tokio::io::AsyncBufReadExt;
 use tracing::{error, info};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt::time::OffsetTime;
-use songbird::SerenityInit;
 use serenity::prelude::*;
 use structopt::StructOpt;
 
+#[allow(clippy::single_match)]
 #[tokio::main]
 async fn main() {
     let opt = opt::Opt::from_args();
@@ -48,9 +49,10 @@ async fn main() {
 
     let token = std::env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    let mut client = Client::builder(token, GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT)
+    let voice = songbird::Songbird::serenity();
+    let builder = Client::builder(token, GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT);
+    let mut client = songbird::serenity::register_with(builder, voice.clone())
         .event_handler(Handler::default())
-        .register_songbird()
         .await
         .expect("Error creating client");
 
@@ -61,12 +63,45 @@ async fn main() {
         data.insert::<ConnectedChannel>(Arc::new(Mutex::new(HashMap::new())));
     }
 
+    let (tx_stdout, mut rx_stdout) = tokio::sync::mpsc::unbounded_channel::<String>();
+    let (tx_exit, rx_exit) = tokio::sync::oneshot::channel();
+
+    // コンソールから標準入力を受け取ってチャネルに送るスレッド
     tokio::spawn(async move {
-        if let Err(why) = client.start().await {
-            error!("Client error: {why:?}");
+        let stdin = tokio::io::stdin();
+        let mut lines = tokio::io::BufReader::new(stdin).lines();
+        while let Some(line) = lines.next_line().await.unwrap() {
+            let _ = tx_stdout.send(line);
         }
     });
 
-    let _ = tokio::signal::ctrl_c().await;
-    info!("Received Ctrl-C, shutting down.");
+    // 標準入力をチャネルから受け取ってexitを処理する
+    tokio::spawn(async move {
+        while let Some(line) = rx_stdout.recv().await {
+            match line.as_str() {
+                "stop" => {
+                    tx_exit.send(()).unwrap();
+                    break;
+                },
+                _ => {}
+            }
+        }
+    });
+
+    tokio::select! {
+        result = client.start() => {
+            if let Err(why) = result {
+                error!("Client error: {why:?}");
+            }
+        }
+        _ = rx_exit => {
+            for &guild in &client.cache_and_http.cache.guilds() {
+                let _ = voice.leave(guild).await;
+            }
+            client.shard_manager.lock().await.shutdown_all().await;
+            println!("Press Enter");
+        }
+    }
+
+    info!("shutting down.");
 }
